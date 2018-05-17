9e30596f03f52e35a6fd8f330a088fd116b20188
clintval <valentine.clint@gmail.com>
2018-05-17 14:05:40 -0700

    Version bump to 0.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as handle:
 
 PACKAGE = 'sample_sheet'
 PACKAGE_NAME = 'sample-sheet'
-VERSION = '0.6.0'
+VERSION = '0.7.0'
 
 URL = f'https://github.com/clintval/{PACKAGE_NAME}'
 ARTIFACT = f'https://github.com/clintval/{PACKAGE_NAME}/archive/v{VERSION}.tar.gz'  # noqa
