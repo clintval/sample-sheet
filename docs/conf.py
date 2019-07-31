a9617c3bacb3b29010be702318919f62b7d0305e
clintval <valentine.clint@gmail.com>
2019-07-31 11:09:46 -0700

    Revision to 0.9.2

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Clint Valentine'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.0'
+release = '0.9.2'
 
 # -- General configuration ---------------------------------------------------
 
