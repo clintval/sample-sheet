72f0496bef58f49532a7c805b8679968e1c9fb8e
Clint Valentine <valentine.clint@gmail.com>
2019-08-19 15:05:35 -0700

    Bugfix: successfully catch import errors for all the smart_open regressions

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Clint Valentine'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.2'
+release = '0.9.3'
 
 # -- General configuration ---------------------------------------------------
 
