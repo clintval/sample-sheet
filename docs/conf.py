8da5c6b5ef69170c456b6e7b7673fc2d57536fa2
Clint Valentine <valentine.clint@gmail.com>
2019-08-20 00:31:59 -0700

    Possibly allow install on Windows (#91)

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Clint Valentine'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.3'
+release = '0.9.4'
 
 # -- General configuration ---------------------------------------------------
 
