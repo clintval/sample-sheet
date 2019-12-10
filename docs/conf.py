92206db6e2fb20d21611fc4ee2fae12b5204cf4a
Clint Valentine <valentine.clint@gmail.com>
2019-12-09 23:44:22 -0500

    Version bump to 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Clint Valentine'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
+release = '0.11.0'
 
 # -- General configuration ---------------------------------------------------
 
